c485ec3d9c6234699df51943d248265ccc11ac33
YashUbana <yashubana789@gmail.com>
2022-12-20 15:06:33 +0530

    Array with n input

---
+++ src/ArrayWithN.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArrayWithN {
+    public static void main(String[] args) {
+        Scanner a = new Scanner(System.in);
+        System.out.print("Enter the number of element for array(2-19): ");
+        int n = a.nextInt();
+        while(n<1 || n >20){
+            System.out.println("Invalid input. Try Again");
+            n = a.nextInt();
+        }
+        int[] arr = new int[n];
+        System.out.print("Enter the array with spaces: ");
+        for (int i = 0; i<n; i++){
+            int x = a.nextInt();
+            arr[i] = x;
+        }
+        System.out.print(Arrays.toString(arr));
+    }
+}
