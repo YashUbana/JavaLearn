d657758741a9643465dae98b21329579d7c64967
YashUbana <yashubana789@gmail.com>
2022-12-20 14:26:45 +0530

    Adding Program

---
+++ src/ToStringMethod.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class ToStringMethod {
+    public static void main(String[] args) {
+        int[] num = {1,2,3,1,4,};
+        System.out.print(Arrays.toString(num));
+        System.out.println();
+    }
+}
