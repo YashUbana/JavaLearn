d2f9d324f1a357bacd3ef7fe7bd609b8978bccaf
Yash Ubana <yashubana789@gmail.com>
2022-12-23 14:44:07 +0530

    Array with n input

---
+++ src/ArrayListsUniqueSort.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class ArrayListsUniqueSort {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the number of ArrayList :");
+        int a = in.nextInt();
+        ArrayList <Integer>inputs = new ArrayList<>();
+
+        for (int i = 0; i < a; i++) {
+            int temp = in.nextInt();
+            if (!inputs.contains(temp)){
+                inputs.add(temp);
+            }
+        }
+        Collections.sort(inputs);
+        System.out.println(inputs);
+
+    }
+}
